682e95d3f9b1aeff9c5a1a7087e44b0eb1e73577
tcaras <trrenty@gmail.com>
2022-01-07 12:30:08 +0200

    Fixed regressions created by #7

---
+++ webjar/src/main/webjar/pdfviewer-customizations.js
@@ -0,0 +1,22 @@
+(function() {
+   window.addEventListener('load', function() {
+      setExternalLinkTarget();
+      setViewRights();
+   }, true);
+
+  // PDFVIEWER-2: Open the links inside the PDF documents in a new tab
+  function setExternalLinkTarget() {
+    PDFViewerApplication.preferences.set('externalLinkTarget', 2);
+  }
+  // PDFVIEWER-13: Allow author of the macro to delegate its view right on the PDF document.
+  function setViewRights() {
+    var file = new URLSearchParams(window.location.search).get('file') || '';
+    var viewerOnly = file.indexOf('PDFViewerService') != -1;
+    if (viewerOnly) {
+      PDFViewerApplication.appConfig.toolbar.download.hidden = true;
+      PDFViewerApplication.appConfig.toolbar.print.hidden = true;
+      PDFViewerApplication.appConfig.secondaryToolbar.downloadButton.hidden = true;
+      PDFViewerApplication.appConfig.secondaryToolbar.printButton.hidden = true;
+    }
+  }
+})();
