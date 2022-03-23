b812ebc13323990e0c1e10cabbe32feec5938445
tcaras <trrenty@gmail.com>
2022-03-23 20:35:10 +0200

    Add a copyable link of the PDF file location in the macro toolbar #40
    * Clone a button from each toolbar and modify it with the proper data
    * Bundle a new image for the copy button in the webjar module
    * Through maven, modify the viewer.css file to add a new style for the added buttons to display the aforementioned image
    * Add the event listeners for the buttons in JavaScriptExtension
    * Update the pom of the ui module to have requirejs and jquery as dependencies

--- webjar/src/main/webjar/pdfviewer-customizations.js
+++ webjar/src/main/webjar/pdfviewer-customizations.js
@@ -2,8 +2,29 @@
    window.addEventListener('load', function() {
       setExternalLinkTarget();
       setViewRights();
+      addCopyButton();
    }, true);
+  // #40: Add a copyable link of the PDF file location in the macro toolbar
+  function addCopyButton() {
+	  var toolbar = document.getElementById('toolbarViewerRight');
+    var secondaryToolbar = document.getElementById('secondaryToolbar');
 
+    var linkButton = document.getElementById('print').cloneNode(true);
+    var secondaryLinkButton = document.getElementById('secondaryPrint').cloneNode(true);
+
+    linkButton.setAttribute('id', 'copy');
+    linkButton.classList.add('copy');
+    linkButton.classList.remove('print');
+    linkButton.removeAttribute('data-l10n-id');
+
+    secondaryLinkButton.setAttribute('id', 'secondaryCopy');
+ 	  secondaryLinkButton.classList.add('copy');
+    secondaryLinkButton.classList.remove('print');
+    secondaryLinkButton.removeAttribute('data-l10n-id');
+
+    toolbar.prepend(linkButton);
+    secondaryToolbar.prepend(secondaryLinkButton);
+  }
   // PDFVIEWER-2: Open the links inside the PDF documents in a new tab
   function setExternalLinkTarget() {
     PDFViewerApplication.preferences.set('externalLinkTarget', 2);
